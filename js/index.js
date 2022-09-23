7e5a1c2763a572095f12412ea08aeb4f80aeb82c
mdshahjalal5 <muhommodshahjalal@gmail.com>
2022-09-23 15:34:47 +0600

    5th dynamically signle card added

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const displayCategories = async(AllNews)=>{
     AllNews.forEach(singleNews => {
         
         const {category_id, category_name} = singleNews;
-        console.log(category_name)
+        // console.log(category_name)
         const li = document.createElement('li');
         li.innerHTML = `
         <a class="nav-link active" aria-current="page" href="#">${category_name}</a>
@@ -21,3 +21,34 @@ const displayCategories = async(AllNews)=>{
 }
 
 loadCategories();
+
+
+const loadNewsDetail = async category_id =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/news/category/02');
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
+
+const displayLoadDetails = async() =>{
+    const data = await loadNewsDetail();
+    const newsDetail = document.getElementById('news-detail');
+    const newsDiv = document.createElement('div');
+    newsDiv.classList.add('row')
+    newsDiv.innerHTML = `
+    <div class="col-md-4">
+    <img src="..." class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+    `;
+    newsDetail.appendChild(newsDiv);
+}
+loadNewsDetail();
+
+displayLoadDetails();
