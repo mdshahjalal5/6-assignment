5c5e7c6dc1efb15984c85dde7d445ac6477b7cfc
mdshahjalal5 <muhommodshahjalal@gmail.com>
2022-09-23 18:46:34 +0600

    6th  dynamically one category details display done

--- js/index.js
+++ js/index.js
@@ -32,23 +32,65 @@ const loadNewsDetail = async category_id =>{
 
 const displayLoadDetails = async() =>{
     const data = await loadNewsDetail();
-    const newsDetail = document.getElementById('news-detail');
-    const newsDiv = document.createElement('div');
-    newsDiv.classList.add('row')
-    newsDiv.innerHTML = `
-    <div class="col-md-4">
-    <img src="..." class="img-fluid rounded-start" alt="...">
-  </div>
-  <div class="col-md-8">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-    `;
-    newsDetail.appendChild(newsDiv);
+    const singleNews = data.data.forEach(singleNews =>{
+        console.log(singleNews);
+        const {title, image_url, details, rating,thumbnail_url, total_view, _id, author, } = singleNews;
+        console.log('title', title);
+        console.log('image url', image_url);
+        console.log('details', details);
+        console.log('rating', rating);
+        console.log('thumbnail_url', thumbnail_url);
+        console.log('total_view', total_view);
+        console.log('_id', _id);
+        console.log('author', author);
+        console.log(author.published_date, 'publish date')
+        const newsDetail = document.getElementById('news-detail');
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('row', 'g-5', 'mb-4')
+        newsDiv.innerHTML = `
+        <div class="col-md-4">
+        <img src=" ${thumbnail_url} " class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5> </br>
+          <p class="card-text">${details.length >100 ? details.slice(0,100) : details}
+            </br>  </br>
+            ${ details.length> 500? details.slice(101,500) + '...' :details}
+          </p>
+          
+          <div class="row">
+            <div class="col-md-4 d-flex justify-content-around">
+                <img src="${image_url}" style="max-width:40px" alt="" class='rounded-circle'>
+                <div>
+                    <h5>Jane Cooper</h5>
+                    <p>${author.published_date.split(' ')[0]} </p>
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <h3> <span><i class="fa-solid fa-eye"></i></span> &nbsp;${total_view ? total_view + ' M': 'N/a'}</h3>
+            </div>
+            <div class="col-md-3">
+                    <span><i class="fa-regular fa-star-half-stroke"></i></span>
+                    <span><i class="fa-regular fa-star"></i></span>
+                    <span><i class="fa-regular fa-star"></i></span>
+                    <span><i class="fa-regular fa-star"></i></span>
+                    <span><i class="fa-regular fa-star"></i></span>
+            </div>
+            <div class="col-md-2">
+                <span><i class="fa-sharp fa-solid fa-arrow-right"></i></span>
+            </div>
+            <div class="text-center">
+                <div class="btn btn-primary w-25 mt-4 text-center">Details</div>
+            </div>
+          </div>
+        </div>
+      </div>
+        `;
+        newsDetail.appendChild(newsDiv);
+    })
 }
-loadNewsDetail();
+
 
 displayLoadDetails();
